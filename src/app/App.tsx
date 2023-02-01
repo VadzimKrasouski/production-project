d924706238ed081792a71c8e0bcb4877646ef3e3
Vadim Krasouski <v.krasouski@giap.pl>
2023-02-01 18:17:02 +0100

    refactored folders tree

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {Link, Route, Routes} from "react-router-dom";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 export const App = () => {
@@ -16,8 +16,8 @@ export const App = () => {
             <Link to={'/about'}>About Page</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
